7071169c084dc22780d5d04e8b236769ab5a143b
dgc7 <dgc7@dgc7.dgc7>
2023-12-23 09:42:51 -0500

    firs configuration ages

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1168,7 +1168,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 45, 45, 400, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 82.41,77.054, 449.44, 99.40 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
